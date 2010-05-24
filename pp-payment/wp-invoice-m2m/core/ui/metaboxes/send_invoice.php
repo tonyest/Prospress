2954849f66a0d14ea7cb95356976cf95138a89df
Brent Shepherd <thenbrent@gmail.com>
2010-05-24 23:49:57 +1000

    Fixed hanging Send Invoice date column; added Today shortcut for date and added "hide-if-no-js" class.

--- pp-payment/wp-invoice-m2m/core/ui/metaboxes/send_invoice.php
+++ pp-payment/wp-invoice-m2m/core/ui/metaboxes/send_invoice.php
@@ -36,33 +36,36 @@ function wp_invoice_metabox_publish($ic) { ?>
 				<th>Due Date</th>
 				<td>
 					<div id="timestampdiv" style="display:block;">
-					<select id="mm" name="wp_invoice[due_date_month]">
-					<option></option>
-					<option value="1" <?php if($ic->due_date_month == '1') echo " selected='selected'";?>>Jan</option>
-					<option value="2" <?php if($ic->due_date_month == '2') echo " selected='selected'";?>>Feb</option>
-					<option value="3" <?php if($ic->due_date_month == '3') echo " selected='selected'";?>>Mar</option>
-					<option value="4" <?php if($ic->due_date_month == '4') echo " selected='selected'";?>>Apr</option>
-					<option value="5" <?php if($ic->due_date_month == '5') echo " selected='selected'";?>>May</option>
-					<option value="6" <?php if($ic->due_date_month == '6') echo " selected='selected'";?>>Jun</option>
-					<option value="7" <?php if($ic->due_date_month == '7') echo " selected='selected'";?>>Jul</option>
-					<option value="8" <?php if($ic->due_date_month == '8') echo " selected='selected'";?>>Aug</option>
-					<option value="9" <?php if($ic->due_date_month == '9') echo " selected='selected'";?>>Sep</option>
-					<option value="10" <?php if($ic->due_date_month == '10') echo " selected='selected'";?>>Oct</option>
-					<option value="11" <?php if($ic->due_date_month == '11') echo " selected='selected'";?>>Nov</option>
-					<option value="12" <?php if($ic->due_date_month == '12') echo " selected='selected'";?>>Dec</option>
-					</select>
-					<input type="text" id="jj" name="wp_invoice[due_date_day]" value="<?php echo $ic->due_date_day; ?>" size="2" maxlength="2" autocomplete="off" />, 
-					<input type="text" id="aa" name="wp_invoice[due_date_year]" value="<?php echo $ic->due_date_year; ?>" size="4" maxlength="5" autocomplete="off" />
-					<div>
-						<span onclick="wp_invoice_add_time(1);" class="wp_invoice_click_me">Today</span> | 
-						<span onclick="wp_invoice_add_time(7);" class="wp_invoice_click_me">In One Week</span> | 
-						<span onclick="wp_invoice_add_time(30);" class="wp_invoice_click_me">In 30 Days</span> |
-						<span onclick="wp_invoice_add_time('clear');" class="wp_invoice_click_me">Clear</span>
+						<select id="mm" name="wp_invoice[due_date_month]">
+							<option></option>
+							<option value="1" <?php if($ic->due_date_month == '1') echo " selected='selected'";?>>Jan</option>
+							<option value="2" <?php if($ic->due_date_month == '2') echo " selected='selected'";?>>Feb</option>
+							<option value="3" <?php if($ic->due_date_month == '3') echo " selected='selected'";?>>Mar</option>
+							<option value="4" <?php if($ic->due_date_month == '4') echo " selected='selected'";?>>Apr</option>
+							<option value="5" <?php if($ic->due_date_month == '5') echo " selected='selected'";?>>May</option>
+							<option value="6" <?php if($ic->due_date_month == '6') echo " selected='selected'";?>>Jun</option>
+							<option value="7" <?php if($ic->due_date_month == '7') echo " selected='selected'";?>>Jul</option>
+							<option value="8" <?php if($ic->due_date_month == '8') echo " selected='selected'";?>>Aug</option>
+							<option value="9" <?php if($ic->due_date_month == '9') echo " selected='selected'";?>>Sep</option>
+							<option value="10" <?php if($ic->due_date_month == '10') echo " selected='selected'";?>>Oct</option>
+							<option value="11" <?php if($ic->due_date_month == '11') echo " selected='selected'";?>>Nov</option>
+							<option value="12" <?php if($ic->due_date_month == '12') echo " selected='selected'";?>>Dec</option>
+						</select>
+						<input type="text" id="jj" name="wp_invoice[due_date_day]" value="<?php echo $ic->due_date_day; ?>" size="2" maxlength="2" autocomplete="off" />, 
+						<input type="text" id="aa" name="wp_invoice[due_date_year]" value="<?php echo $ic->due_date_year; ?>" size="4" maxlength="5" autocomplete="off" />
 					</div>
-					</div> 
 				</td>
 			</tr>
-
+			<tr class="hide-if-no-js">
+				<th colspan="2">
+					<div>
+						<span onclick="wp_invoice_add_time(1);" class="wp_invoice_click_me"><?php _e( 'Today', 'prospress' ); ?></span> | 
+						<span onclick="wp_invoice_add_time(7);" class="wp_invoice_click_me"><?php _e( 'In One Week', 'prospress' ); ?></span> | 
+						<span onclick="wp_invoice_add_time(30);" class="wp_invoice_click_me"><?php _e( 'In 30 Days', 'prospress' ); ?></span> |
+						<span onclick="wp_invoice_add_time('clear');" class="wp_invoice_click_me"><?php _e( 'Clear', 'prospress' ); ?></span>
+					</div>
+				</th>
+			</tr>
 		</table>
 		</div>
 		<div class="clear"></div>
