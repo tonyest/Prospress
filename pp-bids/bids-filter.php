4dc4373202fc091546cb60ed15c4b2945556704c
Brent Shepherd <thenbrent@gmail.com>
2010-09-29 15:01:25 +0700

    Fix incompatibility with YARPP.

--- pp-bids/bids-filter.php
+++ pp-bids/bids-filter.php
@@ -73,8 +73,8 @@ class Bid_Filter_Query {
 	static function add_filters( $obj ) {
 		global $market_systems;
 
-		// Overcome YARPP incompatability
-		if( is_array( $obj->query_vars['post_type'] ) )
+		// Fix YARPP incompatibility
+		if( is_array( $obj->query_vars[ 'post_type' ] ) )
 			return;
 
 		// Don't touch the main query or queries for non-Prospress posts
