fc51c92eb6a1a29f03276270c665857d09234630
Brent Shepherd <thenbrent@gmail.com>
2010-10-18 14:31:52 +0700

    Taxonomy default template improvements.

--- pp-posts/pp-taxonomy-auctions.php
+++ pp-posts/pp-taxonomy-auctions.php
@@ -9,15 +9,8 @@ Template Name: Auctions Taxonomy Index
  * @subpackage Theme
  * @since 0.1
  */
-global $wp_query;
+wp_enqueue_style( 'prospress',  PP_CORE_URL . '/prospress.css' );
 
-$taxonomy = esc_attr( get_query_var( 'taxonomy' ) );
-$tax = get_taxonomy( $taxonomy );
-$term = esc_attr( get_query_var( 'term' ) );
-$term = get_term_by( 'slug', $term, $taxonomy );
-$term_description = term_description( $term->term_id, $taxonomy );
-$term = $term->name;
-$tax_title = sprintf( __( '%s %s' ), $term, $tax->labels->name );
 ?>
 <?php get_header(); ?>
 	<div id="container" class="prospress-container">
@@ -37,6 +30,8 @@ $tax_title = sprintf( __( '%s %s' ), $term, $tax->labels->name );
 
 			<div class="pp-post">
 				<div class="pp-post-content">
+					<div class="pp-end"><?php the_post_end_time( $the_ID, 2, '<br/>' ); ?></div>
+					<div class="pp-price"><?php $market->the_winning_bid_value(); ?></div>
 					<h2 class="pp-title entry-title">
 						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
 							<?php the_title(); ?>
@@ -44,14 +39,15 @@ $tax_title = sprintf( __( '%s %s' ), $term, $tax->labels->name );
 					</h2>
 					<div class="pp-excerpt">
 						<?php the_excerpt(); ?>
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+							<?php printf( __( 'View %s &raquo', 'prospress' ), $market->labels[ 'singular_name' ] ); ?>
+						</a>
 					</div>
 					<div class="pp-publish-details">
 						<?php  _e( 'Published: ', 'prospress' ); the_time('F jS, Y'); ?>
 						<?php _e( 'by ', 'prospress'); the_author(); ?>
 					</div>
 				</div>
-				<div class="pp-end"><?php the_post_end_time( $the_ID, 3, '<br/>' ); ?></div>
-				<div class="pp-price"><?php $market->the_winning_bid_value(); ?></div>
 			</div>
 
 			<?php endwhile; else: ?>
