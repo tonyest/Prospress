4652bc5fd1fe5572ca48e69b0d8d1c44422e4594
Brent Shepherd <thenbrent@gmail.com>
2010-11-10 00:31:04 +0700

    Adding template tag for winning_bid_value

--- pp-bids/pp-bids-templatetags.php
+++ pp-bids/pp-bids-templatetags.php
@@ -15,3 +15,18 @@ function the_bid_form( $post_id = '' ) {
 	$market->the_bid_form();
 }
 
+/**
+ * Print the winning bid value for a post. If no post id is specified, the global post object
+ * is used allowing this function to be used within a loop.
+ **/
+function the_winning_bid_value( $post_id = '' ) {
+	global $post, $market_systems;
+
+	if ( empty( $post_id ) )
+		$post_id = $post->ID;
+
+	$market = $market_systems[ get_post_type( $post_id ) ];
+
+	$market->the_winning_bid_value();
+}
+
