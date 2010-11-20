74695b9d595571c0e9ad909886c998cc66feba42
Brent Shepherd <thenbrent@gmail.com>
2010-11-21 00:52:08 +0700

    New cap for editing completed Prospress posts. Not assigned to anyone by default.

--- pp-posts/pp-capabilities.php
+++ pp-posts/pp-capabilities.php
@@ -196,6 +196,8 @@ function pp_map_meta_cap( $caps, $cap, $user_id, $args ){
 			} elseif ( 'trash' == $post->post_status ) {
 				if ('publish' == get_post_meta($post->ID, '_wp_trash_meta_status', true) )
 					$caps[0] = 'edit_published_prospress_posts';
+			} elseif ( 'completed' == $post->post_status ) {
+				$caps[0] = 'edit_completed_prospress_posts';
 			} else {
 				$caps[0] = 'edit_prospress_posts';
 				$caps[] = 'publish_prospress_posts';
