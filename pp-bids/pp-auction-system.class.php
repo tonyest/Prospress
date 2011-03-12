891050e5ec60f311a06fe10437baf0fb333473f7
Tonyest <anthony.y.khoo@gmail.com>
2011-03-12 14:49:50 +0100

    bid form now displays 'Your Listing' as header when publisher views own post as well as hiding all bid-form input elements. All others see as normal. Private posts will be affected by this as only the publisher and administrator may view a private post.

--- pp-bids/pp-auction-system.class.php
+++ pp-bids/pp-auction-system.class.php
@@ -49,10 +49,10 @@ class PP_Auction_Bid_System extends PP_Market_System {
 
 	/**
 	 * Adds bid form fields to the bid form. Done as a separate function to allow for bid form
-	 * to be customised in market system with a filter.
+	 * to be customised in market system with a filter. If viewer is publisher skips input elements
 	 **/
 	protected function bid_form_fields( $post_id = NULL ) { 
-		global $post_ID, $currency_symbol;
+		global $post_ID, $currency_symbol,$user_ID, $post;
 
 		$post_id = ( $post_id === NULL ) ? $post_ID : $post_id;
 		$bid_count = $this->get_bid_count( $post_id );
@@ -66,9 +66,11 @@ class PP_Auction_Bid_System extends PP_Market_System {
 			$bid_bid_form_fields .= '<div id="winning_bidder">' . __("Winning Bidder: ", 'prospress' ) . $this->the_winning_bidder( $post_id, $dont_echo ) . '</div>';
 			$bid_bid_form_fields .= '<div id="current_bid_val">' . __("Current Bid: ", 'prospress' ) . $this->the_winning_bid_value( $post_id, $dont_echo ) . '</div>';
 		}
-		$bid_bid_form_fields .= '<label for="bid_value" class="bid-label">' . __( 'Enter max bid: ', 'prospress' ) . $currency_symbol . ' </label>';
-		$bid_bid_form_fields .= '<input type="text" aria-required="true" tabindex="1" size="8" value="" id="bid_value" name="bid_value"/>';
-		
+		if (  $post->post_author != $user_ID ){ //viewer is publisher "cannot bid on own auction"
+			$bid_bid_form_fields .= '<label for="bid_value" class="bid-label">' . __( 'Enter max bid: ', 'prospress' ) . $currency_symbol . ' </label>';
+			$bid_bid_form_fields .= '<input type="text" aria-required="true" tabindex="1" size="8" value="" id="bid_value" name="bid_value"/>';
+			$bid_bid_form_fields .= '<input name="bid_submit" type="submit" id="bid_submit" value="' . $this->labels[ 'bid_button' ] .'" />';
+		}
 		return $bid_bid_form_fields;
 	}
 
--- pp-bids/pp-market-system.class.php
+++ pp-bids/pp-market-system.class.php
@@ -43,7 +43,10 @@ abstract class PP_Market_System {
 		$this->name = sanitize_user( $name, true );
 
 		$defaults = array(
-						'bid_form_heading' 	=> __( 'Place Bid', 'prospress' ),
+						'bid_form_heading' 	=> array( 
+											'general' => __( 'Place Bid', 'prospress' ), 
+											'publisher' => __( 'Your Listing', 'prospress' ) 
+											),
 						'description'		=> '',
 						'label' 			=> ucfirst( $this->name ),
 						'labels' 			=> array(
@@ -67,7 +70,7 @@ abstract class PP_Market_System {
 						'bid_object_name' 	=> $this->name . '-bids',
 						'post_object_type' 	=> 'PP_Post'
 						);
-
+			
 		$args = wp_parse_args( $args, $defaults );
 
 		$this->label 				= $args[ 'label' ];
@@ -162,19 +165,21 @@ abstract class PP_Market_System {
 
 	// The function that brings all the bid form elements together.
 	public function bid_form( $post_id = NULL ) {
-		global $post;
+		global $post, $user_ID;
 
 		$post_id = ( $post_id === NULL ) ? $post->ID : $post_id;
 		$the_post = ( empty ( $post ) ) ? get_post( $post_id) : $post;
 
 		if ( $this->is_post_valid( $post_id ) ) {
 			$form = '<form id="bid_form-' . $post_id . '" class="bid-form" method="post" action="">';
-			$form .= '<h4>' . $this->bid_form_heading . '</h4>';
+			if ( $post->post_author == $user_ID )
+				$form .= '<h4>' . $this->bid_form_heading[publisher] . '</h4>';
+			else
+				$form .= '<h4>' . $this->bid_form_heading[general] . '</h4>';
 			$form .= '<div class="bid-updated bid_msg" >' . $this->get_message() . '</div><div>';
 			$form .= $this->bid_form_fields( $post_id );
 			$form .= wp_nonce_field( __FILE__, 'bid_nonce', false, false );
 			$form .= '<input type="hidden" name="post_ID" value="' . $post_id . '" id="post_ID" /> ';
-			$form .= '<input name="bid_submit" type="submit" id="bid_submit" value="' . $this->labels[ 'bid_button' ] .'" />';
 			$form .= '</div></form>';
 		} else {
 			$form = '<div id="bid_form-' . $post_id . '" class="bid-form">';
